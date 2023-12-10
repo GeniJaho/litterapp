6285de1a6722e4b9d3b493458245223e88f1f2b6
fabjola_memushaj <jolamemushaj@gmail.com>
2023-12-10 22:45:04 +0100

    Improved tags seeder

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -12,6 +12,25 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
-        Tag::factory()->count(100)->create();
+        $now = now();
+        Tag::query()->insert([
+            //brands
+            ['name' => 'Acadia', 'slug' => 'acadia', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Applegreen', 'slug' => 'applegreen', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Asahi', 'slug' => 'asahi', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Avoca', 'slug' => 'avoca', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Bacardi', 'slug' => 'bacardi', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Ballygowan', 'slug' => 'ballygowan', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Bewleys', 'slug' => 'bewleys', 'created_at' => $now, 'updated_at' => $now],
+
+            //materials
+            ['name' => 'Aluminium', 'slug' => 'aluminium', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Bronze', 'slug' => 'bronze', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Copper', 'slug' => 'copper', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Fiberglass', 'slug' => 'fiberglass', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Glass', 'slug' => 'glass', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Iron or Steel', 'slug' => 'iron_or_steel', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Wood', 'slug' => 'wood', 'created_at' => $now, 'updated_at' => $now],
+        ]);
     }
 }
