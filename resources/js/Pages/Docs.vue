4971e68bbdb89b82d8920d1ac286638f661bb08d
geni_jaho <jahogeni@gmail.com>
2023-12-26 11:52:16 +0100

    Small text clarification

--- resources/js/Pages/Docs.vue
+++ resources/js/Pages/Docs.vue
@@ -17,7 +17,7 @@ import UploadPhotos from "@/Components/UploadPhotos.vue";
 
                         <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
                             <p class="text-gray-500 dark:text-gray-400 leading-relaxed">
-                                Documentation will appear here soon.
+                                Documentation will appear here soon, or somewhere else...
                             </p>
                         </div>
 
