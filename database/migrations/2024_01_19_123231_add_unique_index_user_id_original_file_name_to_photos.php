807019a96fcec4965a43d7626ee5dc9adfd1711b
geni_jaho <jahogeni@gmail.com>
2024-01-20 19:45:38 +0100

    Fix migrations

--- database/migrations/2024_01_19_123231_add_unique_index_user_id_original_file_name_to_photos.php
+++ database/migrations/2024_01_19_123231_add_unique_index_user_id_original_file_name_to_photos.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Console\Commands\FillOriginalFileNames;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,9 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->string('original_file_name')->after('path');
+            $table->unique(['user_id', 'original_file_name']);
         });
-
-        Artisan::call(FillOriginalFileNames::class);
     }
 };
