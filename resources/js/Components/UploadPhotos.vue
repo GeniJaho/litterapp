1c4188f1a6c863c9c9ce711ff5e0d72f98a4dcd2
geni_jaho <jahogeni@gmail.com>
2024-01-20 19:14:23 +0100

    Show duplicate photos as errors

--- resources/js/Components/UploadPhotos.vue
+++ resources/js/Components/UploadPhotos.vue
@@ -31,7 +31,7 @@ const FilePond = vueFilePond(
 
 const server = {
     url: '.', // current host
-    process: {
+    procezz: {
         url: '/upload',
         method: 'POST',
         withCredentials: false,
@@ -43,6 +43,57 @@ const server = {
         onerror: null,
         ondata: null,
     },
+    process: (fieldName, file, metadata, load, error, progress, abort) => {
+        // fieldName is the name of the input field
+        // file is the actual file object to send
+        const formData = new FormData();
+        formData.append(fieldName, file, file.name);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', '/upload');
+        request.setRequestHeader('X-CSRF-TOKEN', page.props.csrf_token);
+        request.setRequestHeader('X-Inertia', '1');
+
+        request.timeout = 120000; // 2 minutes
+
+        // Should call the progress method to update the progress to 100% before calling load
+        // Setting computable to false switches the loading indicator to infinite mode
+        request.upload.onprogress = (e) => {
+            progress(e.lengthComputable, e.loaded, e.total);
+        };
+
+        // Should call the load method when done and pass the returned server file id
+        // this server file id is then used later on when reverting or restoring a file
+        // so your server knows which file to return without exposing that info to the client
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 300) {
+                if (request.status === 200 && request.responseText.includes('\"errors\":{\"photo\":')) {
+                    error(JSON.parse(request.responseText).props.errors.photo);
+                    return;
+                }
+
+                // the load method accepts either a string (id) or an object
+                load(request.responseText);
+            } else {
+                // Can call the error method if something is wrong, should exit after
+                error('oh no');
+            }
+        };
+
+        request.send(formData);
+
+        // Should expose an abort method so the request can be cancelled
+        return {
+            abort: () => {
+                // This function is entered if the user has tapped the cancel button
+                request.abort();
+
+                // Let FilePond know the request has been cancelled
+                abort();
+            },
+        };
+    },
+
     fetch: null,
     revert: null,
 };
