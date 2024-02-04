934f715ddb1e9c44fbe4079e774066ff30c4bdb9
fabjola_memushaj <jolamemushaj@gmail.com>
2024-02-04 20:44:13 +0100

    Dashboard new design

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -1,16 +1,78 @@
 <script setup>
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import {Link} from "@inertiajs/vue3";
 </script>
 
 <template>
     <div>
-        <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
+        <div class="">
             <h1 class="text-2xl font-medium text-gray-900 dark:text-white">
-                Welcome to Littertagger!
+                Welcome to LitterApp!
             </h1>
 
-            <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                It's in a very Work In Progress state right now.
-            </p>
+            <div class="flex flex-col space-y-12 lg:p-12">
+                <p class="mt-6 text-gray-600 dark:text-white leading-relaxed">
+                    It's in a very Work In Progress state right now.
+                </p>
+
+                <div class="">
+                    <div class="text-sm py-2 dark:text-white">Scroll down to see how to</div>
+                    <Link :href="route('my-photos')">
+                    <PrimaryButton class="uppercase">Start uploading</PrimaryButton>
+                    </Link>
+                </div>
+
+                <div class="flex flex-col pl-4 space-y-20">
+                    <div>
+                        <div class="flex flex-row items-center space x-4">
+                            <div class="w-1.5 h-1.5  bg-gray-500 dark:bg-white rounded-full"></div>
+                            <div class="text-2xl text-gray-600 dark:text-white pl-4">1</div>
+                        </div>
+                        <div class="text-gray-600 dark:text-white text-2xl">
+                            Take a <strong>picture</strong> of the litter.
+                        </div>
+                        <div>
+                            <div class="text-gray-600 dark:text-white">
+                                GPS location is recommended.
+                            </div>
+                            <div class="text-gray-600 dark:text-white text-xs">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex flex-row items-center space x-4">
+                            <div class="w-1.5 h-1.5  bg-gray-500 dark:bg-white rounded-full"></div>
+                            <div class="text-2xl text-gray-600 dark:text-white  pl-4">2</div>
+                        </div>
+                        <div class="text-gray-600 dark:text-white text-2xl">
+                            <strong>Upload</strong> it into your photos.
+                        </div>
+                        <div class="text-gray-600 dark:text-white">
+                            Access your library of lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex flex-row items-center space x-4">
+                            <div class="w-1.5 h-1.5  bg-gray-500 dark:bg-white rounded-full"></div>
+                            <div class="text-2xl text-gray-600 dark:text-white  pl-4">3</div>
+                        </div>
+                        <div class="text-gray-600 dark:text-white text-2xl">
+                            Open it and <strong>register data</strong>.
+                        </div>
+                        <div class="text-gray-600 dark:text-white">
+                            Add lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
 
     </div>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,7 @@ import Welcome from '@/Components/Welcome.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div>
                     <Welcome />
                 </div>
             </div>
