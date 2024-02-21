2efa6a2de8fdcf286212e4838a406b01b825e0a6
geni_jaho <jahogeni@gmail.com>
2024-02-21 20:55:22 +0100

    Small CSS fix

--- resources/js/Pages/Photo/PhotoItem.vue
+++ resources/js/Pages/Photo/PhotoItem.vue
@@ -131,12 +131,12 @@ const selectedSizeTag = ref(props.tags.size[0]);
                     </PrimaryButton>
                 </div>
 
-                <div class="mt-4 text-sm text-gray-500 flex flex-wrap space-x-1">
+                <div class="mt-4 text-sm text-gray-500 flex flex-wrap gap-1">
                     <span
                         v-for="tag in item.pivot.tags"
                         :key="tag.id"
                         @click="$emit('remove-tag-from-item', item.pivot, tag.id)"
-                        class="inline-flex cursor-pointer items-center gap-x-1.5 rounded-full px-2 py-1 mb-2 mr-2 text-xs font-medium text-gray-900 dark:text-gray-100 ring-1 ring-inset ring-gray-200"
+                        class="inline-flex cursor-pointer items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium text-gray-900 dark:text-gray-100 ring-1 ring-inset ring-gray-200"
                     >
                         <svg class="h-1.5 w-1.5 fill-green-500" viewBox="0 0 6 6"
                              aria-hidden="true"><circle cx="3" cy="3" r="3"/></svg>
