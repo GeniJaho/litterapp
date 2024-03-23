931630b5a0c03c0e78d958d7efd95957a5c81a6d
geni_jaho <jahogeni@gmail.com>
2024-03-23 17:08:14 +0100

    Better dark mode for tag shortcut listings

--- resources/js/Pages/TagShortcuts/Index.vue
+++ resources/js/Pages/TagShortcuts/Index.vue
@@ -72,7 +72,7 @@ provide('tags', readonly(props.tags));
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                         <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
-                            <table class="min-w-full divide-y divide-gray-300">
+                            <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-500">
                                 <thead class="bg-gray-50 dark:bg-gray-800">
                                 <tr>
                                     <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-100 sm:pl-6">Shortcut</th>
@@ -80,7 +80,7 @@ provide('tags', readonly(props.tags));
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-100">Actions</th>
                                 </tr>
                                 </thead>
-                                <tbody class="divide-y divide-gray-200 bg-white dark:bg-gray-700">
+                                <tbody class="divide-y divide-gray-200 dark:divide-gray-600 bg-white dark:bg-gray-700">
                                 <tr
                                     v-for="tagShortcut in filteredTagShortcuts"
                                     :key="tagShortcut.id"
