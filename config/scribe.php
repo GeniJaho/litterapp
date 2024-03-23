9f92afe8b1f8411d292f4a925cae5643e751b0db
geni_jaho <jahogeni@gmail.com>
2024-03-23 17:12:25 +0100

    Include tag shortcuts in api docs

--- config/scribe.php
+++ config/scribe.php
@@ -40,7 +40,7 @@ return [
             // Routes that match these conditions will be included in the docs
             'match' => [
                 // Match only routes whose paths match this pattern (use * as a wildcard to match any characters). Example: 'users/*'.
-                'prefixes' => ['photo*'],
+                'prefixes' => ['photo*', 'user/tag-shortcut*'],
 
                 // Match only routes whose domains match this pattern (use * as a wildcard to match any characters). Example: 'api.*'.
                 'domains' => ['*'],
