2feebae18b8c04ba565b9d0f84cba0ee36459c0d
geni_jaho <jahogeni@gmail.com>
2024-03-28 14:19:54 +0100

    Add zoom component

--- resources/js/Pages/Photos/Index.vue
+++ resources/js/Pages/Photos/Index.vue
@@ -178,7 +178,7 @@ const filter = (filters) => {
                                 class="rounded-lg bg-indigo-600 dark:bg-teal-400"
                             >
                                 <div
-                                    class="relative"
+                                    class="relative overflow-hidden group"
                                     :class="isSelecting && selectedPhotos.includes(photo.id) ? 'scale-[.95]' : ''"
                                 >
                                     <a
@@ -195,10 +195,14 @@ const filter = (filters) => {
                                         >
                                     </a>
 
-                                    <ZoomIcon
+                                    <div
                                         @click="zoomedImage = photo"
-                                        class="absolute top-2 left-2 cursor-pointer"
-                                    />
+                                        class="w-36 h-36 rounded-full cursor-pointer bg-transparent group-hover:bg-black/70 absolute -top-[72px] -left-[72px] transform transform-opacity duration-100"
+                                    >
+                                        <div class="relative w-full h-full">
+                                            <i class="fas fa-magnifying-glass-plus text-white text-xl absolute bottom-7 right-9"></i>
+                                        </div>
+                                    </div>
 
                                     <TaggedIcon v-if="photo.items_exists" class="absolute top-2 right-2" />
 
