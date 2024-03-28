325ba9ea3c79905508cafd0e22028b6e924cfc1f
fabjola_memushaj <jolamemushaj@gmail.com>
2024-03-28 14:54:22 +0100

    Embedded Grafana and changed and changed layout for landing page

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -10,23 +10,38 @@ import {Link} from "@inertiajs/vue3";
                 <h1 class="text-xl leading-8 font-black tracking-widest-xl dark:text-mainWhite text-darkBlue">
                     Welcome to LitterApp!
                 </h1>
-                <div class="mt-16 md:mt-16">
-                    <div class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl dark:text-mainWhite text-darkBlue">
+                <div class="flex flex-col md:flex-row md:space-x-24 md:items-end mt-16 md:mt-16">
+                    <div
+                        class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl dark:text-mainWhite text-darkBlue">
                         From
                         <span class="font-black tracking-widest-xl">waste to data</span>, step by step
                     </div>
-                    <div class="text-base font-normal tracking-widest-xl dark:text-mainWhite text-darkBlue pt-12 pb-4">I
-                        already know the story
+                    <div class="">
+                        <div
+                            class="whitespace-nowrap text-base font-normal tracking-widest-xl dark:text-mainWhite text-darkBlue pt-12 pb-4">
+                            I
+                            already know the story
+                        </div>
+                        <Link :href="route('upload')">
+                            <button
+                                class="whitespace-nowrap inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
+                                Start uploading <i class="fa-solid fa-arrow-right text-base pl-2"></i>
+                            </button>
+                        </Link>
                     </div>
-                    <Link :href="route('upload')">
-                        <button
-                            class="inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
-                            Start uploading <i class="fa-solid fa-arrow-right text-base pl-2"></i>
-                        </button>
-                    </Link>
                 </div>
 
-                <div class="flex flex-col items-center pt-24 pb-12 md:pt-16">
+                <div v-if="$page.props.auth?.user?.id == 1" class="pt-32 w-full">
+                    <iframe class="w-full"
+                            src="https://litterapp.net/grafana/d-solo/aadcc1c0-7a57-4d18-87cb-f13448fb1d45/main-dashboard?orgId=1&from=1709037342679&to=1711629342680&panelId=38"
+                            width="450"
+                            height="350"
+                            frameborder="0"
+                    >
+                    </iframe>
+                </div>
+
+                <div class="flex flex-col items-center pt-24 pb-12 md:pt-32">
                     <div class="h-2 w-2 bg-darkBlue dark:bg-mainWhite rounded-full"></div>
                     <div
                         class="text-center text-2xl leading-8 font-black tracking-widest-xl dark:text-mainWhite text-darkBlue pt-8">
@@ -148,32 +163,33 @@ import {Link} from "@inertiajs/vue3";
                 <div class="h-2 w-2 bg-darkBlue dark:bg-mainWhite rounded-full"></div>
             </div>
 
-            <div class="pt-12 pb-24 md:pb-32">
+            <div class="flex flex-col md:flex-row md:space-x-32 md:items-end pt-12 pb-24 md:pb-32">
+
                 <div class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl dark:text-mainWhite text-darkBlue">
                     Capturing data is <span class="font-black">fun</span>, and crucially <span
                     class="font-black">useful</span>.
                 </div>
 
-<!--                <div class="pt-12">-->
-<!--                    <div class="tracking-widest-xl dark:text-mainWhite text-darkBlue text-lg pb-4">-->
-<!--                        Let's make more success stories:-->
-<!--                    </div>-->
-<!--                    <button-->
-<!--                        class="inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">-->
-<!--                        Our stories-->
-<!--                    </button>-->
-<!--                </div>-->
+                <!--                <div class="pt-12">-->
+                <!--                    <div class="tracking-widest-xl dark:text-mainWhite text-darkBlue text-lg pb-4">-->
+                <!--                        Let's make more success stories:-->
+                <!--                    </div>-->
+                <!--                    <button-->
+                <!--                        class="inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">-->
+                <!--                        Our stories-->
+                <!--                    </button>-->
+                <!--                </div>-->
 
-                <div class="pt-12">
+                <div class="pt-12 md:pt-0">
                     <div class="tracking-widest-xl dark:text-mainWhite text-darkBlue text-lg pb-4">
-                        Read about our Mission and join the ride 😎
+                        Read about our Mission and join the ride:
                     </div>
                     <a
                         href="https://litterapp.net/en/05_Mission-Vision/"
                         target="_blank"
                     >
                         <button
-                            class="inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
+                            class="whitespace-nowrap inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
                             Our Mission
                         </button>
                     </a>
@@ -181,28 +197,29 @@ import {Link} from "@inertiajs/vue3";
             </div>
         </div>
 
-        <div class="px-12 lg:px-40 flex items-center bg-darkBlue dark:bg-mainWhite">
-            <div class="leading-12 text-mainWhite dark:text-darkBlue py-20 md:py-32">
-                <div class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl text-mainWhite dark:text-darkBlue">The
-                    Global community is rising.
+
+        <div
+            class="flex flex-col md:flex-row md:space-x-28 md:items-end py-20 md:py-32 px-12 lg:px-40 bg-darkBlue dark:bg-mainWhite">
+            <div
+                class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl text-mainWhite dark:text-darkBlue">
+                The
+                Global community is rising.
+                Let's
+                <span class="font-black">tag litter,</span> together.
+            </div>
+            <div class="pt-12 md:pt-0">
+                <div class="tracking-widest-xl text-mainWhite dark:text-darkBlue text-lg pb-2">
+                    Interested? Join our slack workspace:
                 </div>
-                <div class="tracking-widest-xl md:leading-12 text-4xl md:text-6xl text-mainWhite dark:text-darkBlue">Let's
-                    <span class="font-black">tag litter,</span> together.
-                </div>
-                <div class="pt-10">
-                    <div class="tracking-widest-xl text-mainWhite dark:text-darkBlue text-lg pb-2">
-                        Interested? Join our slack workspace:
-                    </div>
-                    <a
-                        href="https://join.slack.com/t/helpingearthtogether/shared_invite/zt-2c81hu09y-Edwy2o74yk7FulbHACjnGw"
-                        target="_blank"
-                    >
-                        <button
-                            class="inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
-                            Go to litterapp
-                        </button>
-                    </a>
-                </div>
+                <a
+                    href="https://join.slack.com/t/helpingearthtogether/shared_invite/zt-2c81hu09y-Edwy2o74yk7FulbHACjnGw"
+                    target="_blank"
+                >
+                    <button
+                        class="whitespace-nowrap inline-flex items-center px-4 py-3 bg-turqoFocus border border-transparent rounded-md font-black text-sm tracking-widest-xl text-darkBlue hover:bg-turqo focus:bg-turqoFocus active:bg-turqoFocus focus:outline-none focus:ring-2 focus:ring-turqo focus:ring-offset-2 transition ease-in-out duration-150 uppercase">
+                        Go to litterapp
+                    </button>
+                </a>
             </div>
         </div>
     </div>
