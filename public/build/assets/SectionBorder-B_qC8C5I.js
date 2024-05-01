47273f02b1527e63a41952a1b57f7273aef3f1a8
geni_jaho <jahogeni@gmail.com>
2024-05-01 22:09:44 +0200

    Commit assets

---
+++ public/build/assets/SectionBorder-B_qC8C5I.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as r,d as s,a as e}from"./app-xI8K_SVN.js";const c={},t={class:"hidden sm:block"},a=e("div",{class:"py-8"},[e("div",{class:"border-t border-gray-200 dark:border-gray-700"})],-1),d=[a];function n(_,i){return r(),s("div",t,d)}const f=o(c,[["render",n]]);export{f as S};
