c6bf8034b426e7bb497ba1a0c92fbb7b7913259d
geni_jaho <jahogeni@gmail.com>
2025-01-25 22:58:29 +0100

    Well it's chaotic

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -72,7 +72,7 @@ const alignmentClasses = computed(() => {
                 style="display: none;"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div class="rounded-md ring-1 ring-black/50" :class="contentClasses">
                     <slot name="content" />
                 </div>
             </div>
